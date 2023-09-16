c86b32face2fb389c08a78e6fa629569a5fa2eba
Anil Devidas Janjal <132841194+AnilJanjal@users.noreply.github.com>
2023-09-16 13:23:42 +0530

    Update App.js
    ok

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,29 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 import star from '../star.png'
 const App = () => {
+
+  const [dimensions, setDimensions] = useState({ width: 300, height: 300 });
+
+  // Function to handle the click event and increase the dimensions
+  const handleClick = () => {
+    setDimensions({
+      width: dimensions.width + 2,
+      height: dimensions.height + 2,
+    });
+  };
+  
   return (
     <div id="main">
-      <img src={star}  height="300px" width="300px" />
+      <img
+        src={star}
+        alt="Star"
+        height={dimensions.height + 'px'}
+        width={dimensions.width + 'px'}
+        onClick={handleClick} // Attach the click event handler
+      />
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
